2ff33448be2cf8e8d17c60830a9b1eff1b70f430
Alessasi <ailton.silva@somospi.com.br>
2021-05-03 02:22:44 -0300

    Variables' exercise

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+var numberOfStoplights: Int = 4
+var population: Int
+
+population = 5422
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights"
+
+print(townDescription)
